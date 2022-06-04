ff78b45a19ea69a5f9a18533be65caba7a8123d9
nshishkin <69085430+nshishkin@users.noreply.github.com>
2022-06-04 15:17:14 +0300

    add boilerplate Snake and Cube classes

---
+++ Snake.py
@@ -0,0 +1,60 @@
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+
+class Cube(object):
+    rows = 0
+    w = 0
+
+    def __int__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+
+class Snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def add_cube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+
+def draw_grid(w, rows, surface):
+    pass
+
+
+def redraw_window(surface):
+    pass
+
+
+def random_snack(rows, items):
+    pass
+
+
+def message_box(subject, content):
+    pass
+
+
+def main():
+    pass
+
+rows =
+w=
+h=
