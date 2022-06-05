a777a667f84cfd8edafdbfd330786426fd5bc192
nshishkin <69085430+nshishkin@users.noreply.github.com>
2022-06-05 11:43:40 +0300

    draw empty grid

--- Snake.py
+++ Snake.py
@@ -37,11 +37,21 @@ class Snake(object):
 
 
 def draw_grid(w, rows, surface):
-    pass
+    size_btwn = w // rows
+    x = 0
+    y = 0
+    for i in range(rows):
+        x += size_btwn
+        y += size_btwn
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
 
 
 def redraw_window(surface):
-    pass
+    global rows, width
+    surface.fill((0, 0, 0))
+    draw_grid(width, rows, surface)
+    pygame.display.update()
 
 
 def random_snack(rows, items):
@@ -53,8 +63,19 @@ def message_box(subject, content):
 
 
 def main():
+    global rows, width
+    width = 500
+    rows = 20
+    win = pygame.display.set_mode((width, width))
+    s = Snake((255, 0, 0), (10, 10))
+    flag = True
+    clock = pygame.time.Clock()
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        redraw_window(win)
+
     pass
 
-rows =
-w=
-h=
+
+main()
